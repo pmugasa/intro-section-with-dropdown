c03ecd2887bf870290bc6c10c9e0b812a645e960
Pete Laptop <pmugasa78@gmail.com>
2023-04-22 14:05:18 +0200

    removed image from assets to public

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://pmugasa.github.io/intro-section-with-dropdown",
   plugins: [react()],
-})
+});
